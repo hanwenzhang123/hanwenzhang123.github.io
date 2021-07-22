de854e721bbeda7cb3e61da5d35343c113f1847a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-22 00:07:21 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -16,4 +16,94 @@ const projectInfo = [
         </div>
         `
     },
+    {
+        projectName: 'todo-list',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
+    {
+        projectName: 'newsletter',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
+    {
+        projectName: 'daily-journal',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
+    {
+        projectName: 'keeper-react',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
+    {
+        projectName: 'scoreboard-react-app',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
+    {
+        projectName: 'counseling-site',
+        projectDetails: `
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Project Details</h2>
+                <span class="close">X</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+        </div>
+        </div>
+        `
+    },
 ];
